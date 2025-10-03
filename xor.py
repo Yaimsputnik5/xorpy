e35c9c6774c7be473ffb5143ff6c1347fee7cceb
Shawn Evans <sevans@nopsec.com>
2025-10-03 15:03:53 -0400

    Added a b64 binary key option

--- xor.py
+++ xor.py
@@ -1,56 +1,81 @@
-#!/usr/bin/env python3
-import sys
+#!/usr/bin/env python
 import argparse
-import string
-import random
 import base64
-
-def round_up(number): return int(number) + (number % 1 > 0)
+import sys
 
 def xorcrypt(data, key):
-    try:
-        result = bytearray()
-        pad = key * round_up(len(data)/len(key)) + key[:round_up(len(data)%len(key))]
-        pad.encode('ascii')
-        for i in range(len(data)):
-            result.append(data[i] ^ ord(pad[i]))
-        return bytes(result)
-    except Exception as e:
-        print(e)
+    """
+    Performs XOR encryption/decryption on byte data.
+    """
+    return bytes(a ^ b for a, b in zip(data, key * (len(data) // len(key)) + key[:len(data) % len(key)]))
 
-title = 'Xorpy v1.1'
-author = 'Coded by: Shawn Evans'
-email = 'Email: ShawnDEvans@gmail.com<mailto:ShawnDEvans@gmail.com>'
+def main():
+    """
+    Main function to parse arguments and run the XOR cipher.
+    """
+    parser = argparse.ArgumentParser(
+        prog='Xorpy v1.2',
+        description='Stupid simple XOR encryption/decryption utility.',
+        formatter_class=argparse.RawTextHelpFormatter,
+        epilog='Duct taped together by Shawn Evans - sevans@nopsec.com'
+    )
 
-if __name__ == '__main__':
+    # Mutually exclusive group for key input
+    key_group = parser.add_mutually_exclusive_group(required=True)
+    key_group.add_argument(
+        '-k', '--key',
+        help='Clear-text key string.'
+    )
+    key_group.add_argument(
+        '--bk',
+        help='Base64-encoded binary key string.'
+    )
 
-    parser = argparse.ArgumentParser(prog='Xorpy v1.2', description='Stupid simple XOR encryption utility', epilog='Duct taped together by Shawn Evans - sevans@nopsec.com')
-    parser.add_argument('-o', '--output', help='Output file name, random otherwise.', dest='output_file')
-    parser.add_argument('-k', '--key', help='Secret key, ex "P@ssH0le"', dest='key', required=True)
-    parser.add_argument('input_file', nargs='?')
+    # Optional arguments
+    parser.add_argument(
+        '-o', '--output',
+        help='Output file path for the raw XORd data. If not provided, output will be base64-encoded and printed to stdout.'
+    )
+
+    parser.add_argument(
+        'input_file',
+        nargs='?',
+        type=argparse.FileType('rb'),
+        default=sys.stdin.buffer,
+        help='Input file to read from. If not specified, reads from stdin.'
+    )
 
     args = parser.parse_args()
 
-    try:
-        if not sys.stdin.isatty():
-            inFile = sys.stdin.buffer
-        else:
-            inFile = open(args.input_file, 'rb')
-        key = sys.argv[1]
-    except Exception as e:
-        print(e)
-        parser.print_help()
-        sys.exit()
+    # Read input data
+    input_data = args.input_file.read()
 
-    data = inFile.read()
-    enc_data = xorcrypt(data, args.key)
-    output_file = ''
-    if not args.output_file:
-        rando = ''.join(random.choice(string.ascii_letters) for i in range(10))
-        print(base64.b64encode(enc_data).decode('ascii'))
+    # Determine the key
+    if args.key:
+        key_data = args.key.encode('utf-8')
+    else:  # args.bk
+        try:
+            key_data = base64.b64decode(args.bk)
+        except base64.binascii.Error:
+            print("Error: The base64 key is not valid.", file=sys.stderr)
+            sys.exit(1)
+
+    # Perform the XOR operation
+    result = xorcrypt(input_data, key_data)
+
+    # Handle output
+    if args.output:
+        try:
+            with open(args.output, 'wb') as f:
+                f.write(result)
+            print(f"XOR result written to '{args.output}'")
+        except IOError as e:
+            print(f"Error writing to file: {e}", file=sys.stderr)
+            sys.exit(1)
     else:
-        output_file = args.output_file
-        output = open(f'{output_file}', 'wb')
-        output.write(enc_data)
-        output.close()
+        # Base64 encode the result for printing to stdout
+        encoded_result = base64.b64encode(result).decode('utf-8')
+        print(encoded_result)
 
+if __name__ == "__main__":
+    main()
